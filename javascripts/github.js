085cc2ea40af454f4ada1aa53f335d262bd6de9a
trahair <greg@textkernel.nl>
2012-07-06 12:38:28 +0200

    Changed to use github apiv3

--- javascripts/github.js
+++ javascripts/github.js
@@ -1,5 +1,5 @@
 jQuery.githubUser = function(username, callback) {
-  jQuery.getJSON("http://api.github.com/users/:" + username + "/repos");
+  jQuery.getJSON("https://api.github.com/users/" + username + "/repos");
 }
  
 jQuery.fn.loadRepositories = function(username) {
